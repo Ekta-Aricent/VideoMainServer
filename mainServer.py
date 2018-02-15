6fbfb132af075d26ab9d4e3c7f696488ef3b7b56
Ekta-Aricent <36473855+Ekta-Aricent@users.noreply.github.com>
2018-02-15 17:23:23 +0530

    MainServer

---
+++ mainServer.py
@@ -0,0 +1,31 @@
+import socket
+
+HOST = '0.0.0.0'
+PORT = 12345
+ADDR = (HOST,PORT)
+BUFSIZE = 4096
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind(ADDR)
+server.listen(5)
+
+print 'listening ...'
+
+while True:
+    conn, addr = server.accept()
+    print 'client connected ... ', addr
+    myfile = open('latest.mp4', 'w')
+    
+    while True:
+        data = conn.recv(BUFSIZE)
+        if not data:
+            break
+        myfile.write(data)
+        print 'writing file ....'
+
+    myfile.close()
+    print 'finished writing file'
+    conn.close()
+    print 'client disconnected'
+    break
